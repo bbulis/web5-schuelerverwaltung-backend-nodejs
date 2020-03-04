99ed0e24871ee6ba5457827f098ddf9db4f17b5c
Benjamin Bulis <bbulis@student.tgm.ac.at>
2020-03-04 08:27:56 +0100

    Created Basics for Routing

---
+++ src/app.js
@@ -0,0 +1,33 @@
+import express from "express";
+import bodyParser from "body-parser";
+import "./config/env.config";
+import {buildResponse} from "../helpers/response";
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(bodyParser.json());
+
+app.get("/student", (req, res) => {
+
+});
+
+app.post("/student", (req, res) => {
+
+});
+
+app.get("/student/:id", (req, res) => {
+
+});
+
+app.put("/student/:id", (req, res) => {
+
+});
+
+app.delete("/student/:id", (req, res) => {
+
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}...`)
+});
