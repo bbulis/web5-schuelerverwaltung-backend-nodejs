3dda99ebbb4f4c68d0a3110f236c6b20ff119055
Benjamin Bulis <bbulis@student.tgm.ac.at>
2020-03-05 08:16:04 +0100

    Updated Response for printing Errors

--- src/helpers/response.js
+++ src/helpers/response.js
@@ -1,4 +1,7 @@
-const buildResponse = (successful, res) => {
+const buildResponse = (successful, res, e=null) => {
+    if(e !== null) {
+        console.log(successful, res, e);
+    }
     return {
         successful,
         [successful ? "data": "err"]: res
